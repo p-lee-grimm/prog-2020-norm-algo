50eda4fc56580b1d5ba9df295421eefc8fc2e4de
Komment314 <71181179+Komment314@users.noreply.github.com>
2020-09-19 18:23:48 +0300

    Add files via upload

--- musor.py
+++ musor.py
@@ -1,23 +1,54 @@
-def ReplaceLineInFile(fileName, sourceText, replaceText):
-    file = open(fileName, 'r')
+b = r'C:\Users\User\Desktop\folder1\t1\newfile.txt'
+c = r'C:\Users\User\Desktop\folder1\t1\key.txt'
+count = 0
+
+file = open(c, 'r')
+for line in file.readlines():
+    if line.__contains__("||--"):
+        count += 1
+file.close()
+
+FROM = [0]*count
+TO = [0]*count
+F = 0
+T = 0
+
+file = open(c, 'r')
+for line in file.readlines():
+    if line.__contains__("||--"):
+        FROM[F] = line[4:]
+        F += 1
+file.close()
+
+file = open(c, 'r')
+for line in file.readlines():
+    if line.__contains__("-->>"):
+        TO[T] = line[4:]
+        T += 1
+file.close()
+
+
+def textfind(filename, sourcetext):
+    file = open(filename, 'r')
+    text = file.read()
+    a = text.find(sourcetext)
+    file.close()
+    return a
+
+
+def mpod(filename, sourcetext, replacetext):
+    file = open(filename, 'r')
     text = file.read()
     file.close()
-    file = open(fileName, 'w')
-    file.write(text.replace(sourceText, replaceText))
+    file = open(filename, 'w')
+    file.write(text.replace(sourcetext, replacetext, 1))
     file.close()
-    print ('went well')
 
-n = int(input())
-array = [[0]*n for i in range(3)]
 
-for i in range(0, n):
-    array[i][0],array[i][1] = map(str, input().split())
+for i in range(count):
+    print(FROM[i])
+    print(TO[i])
 
-for i in range(0, n):
-    print(array[i][0], end=" ")
-    print(array[i][1], end="\n")
 
-b = r'C:\Users\User\Desktop\folder1\newfile.txt'
-
-for i in range(n):
-    ReplaceLineInFile(b, array[i][0], array[i][1])
+for i in range(count):
+    mpod(b, FROM[i], TO[i])
